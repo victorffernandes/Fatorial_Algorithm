4577229c56421c89237df1a202ab6d6411e127d7
Victor Fernandes <victorfernandes.nave@gmail.com>
2015-03-08 14:53:55 -0300

    Fatorial
    dds

---
+++ Fatorial_VictorFernandes_2003/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Fatorial_VictorFernandes_2003
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txt = new System.Windows.Forms.TextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Iterativa";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(164, 56);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Recursiva";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txt
+            // 
+            this.txt.Location = new System.Drawing.Point(29, 12);
+            this.txt.Name = "txt";
+            this.txt.Size = new System.Drawing.Size(210, 20);
+            this.txt.TabIndex = 2;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(42, 99);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(35, 13);
+            this.label.TabIndex = 3;
+            this.label.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 140);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.txt);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txt;
+        private System.Windows.Forms.Label label;
+    }
+}
+
---
+++ Fatorial_VictorFernandes_2003/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fatorial_VictorFernandes_2003
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button ite = (sender as Button);
+            Double num = double.Parse(txt.Text);
+            label.Text = Convert.ToString(Iterative(num));
+        }
+
+        private double Iterative(double num)
+        {
+            double result = 1;
+            for (double i = 1; i < num+1; i++)
+            {
+                result *= i;
+            }
+                return result;
+        }
+
+        private double Recursive(double num)
+        {
+            double result;
+            if (num == 0) return 1;
+            result = num * Recursive(num - 1);
+            return result;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Button rec = (sender as Button);
+            Double num = double.Parse(txt.Text);
+            label.Text = Convert.ToString(Recursive(num));
+        }
+
+
+    }
+}
